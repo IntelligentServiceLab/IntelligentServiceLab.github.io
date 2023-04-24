0dd6772d24c1490fe4dc2550caf0d9aed419cdcb
sln <1028406171@qq.com>
2023-04-24 17:25:31 +0800

    add 荣誉

--- js/add_menu_icon.js
+++ js/add_menu_icon.js
@@ -3,13 +3,14 @@ menuIconMap["团队成员"] = "fa-user-tie";
 menuIconMap["学术成果"] = "fa-newspaper";
 menuIconMap["科研项目"] = "fa-flask";
 menuIconMap["联系我们"] = "fa-phone";
-
+menuIconMap["荣获奖励"] = "fa-trophy";
 
 
 
 var hrefMenu = document.getElementsByClassName("site-page");
 for(var i = 0; i < hrefMenu.length; i++)
 {
+
     var menuItemText = hrefMenu[i].innerHTML;
     hrefMenu[i].innerHTML = "<i style=\"padding-right: 1px;\" class=\"fas " + menuIconMap[menuItemText.trim()] + "\"></i>" + menuItemText
     console.log(menuItemText);
